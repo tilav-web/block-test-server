dd82e9fbdf249e28b70d0bf196efc07f3d1bf1ba
tilav-web <shavqiddintilovov0@gmail.com>
2025-06-20 23:41:51 +0500

    changed

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -4,6 +4,7 @@ import { Quiz, QuizSchema } from './quiz.schema';
 import { QuizService } from './quiz.service';
 import { QuizController } from './quiz.controller';
 import { Test, TestSchema } from '../test/test.schema';
+import { AuthModule } from '../user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { Test, TestSchema } from '../test/test.schema';
       { name: Quiz.name, schema: QuizSchema },
       { name: Test.name, schema: TestSchema },
     ]),
+    AuthModule,
   ],
   controllers: [QuizController],
   providers: [QuizService],
